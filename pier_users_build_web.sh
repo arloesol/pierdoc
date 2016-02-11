f6285228d2d32917166e59bd0952f3af16b0323c
John <john@arloesol.com>
2016-02-11 01:34:10 +0100

    remove mydoc cruff and make menu work

--- pier_users_build_web.sh
+++ pier_users_build_web.sh
@@ -7,4 +7,3 @@ jekyll build --config configs/pier/config_users.yml
 echo "done"
 
 echo "All finished building all the web outputs!!!"
-echo "Now push the builds to the server with . pier_4_publish.sh"
